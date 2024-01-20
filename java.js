f2db9f3f3fdbafa732d8ee80567e50da2b3c48a3
Mamadaliev0711 <mamadaliyevsunnatullo@gmail.com>
2024-01-20 23:02:58 +0500

    vazifa bajarildi

---
+++ java.js
@@ -0,0 +1,73 @@
+// 1-masala
+// let num_3 = +prompt("1-masala Ixtiyoriy sonini kiriting");
+// let ac;
+// for (let ac = 1; ac <= num_3; ac += 1) {
+//   console.log(ac);
+// }
+
+// 2-masala
+// let number_2 = +prompt("2-masala Ixtiyoriy sonni kiriting");
+// let sum_2 = 0;
+// let ab;
+// for (ab = 1; ab <= number; ab += 1) {
+//   sum_2 += ab;
+// }
+// console.log("Yig'indi: " + sum_2);
+
+// 3-masala
+// let number_1= +prompt("3-masala Ixtiyoriy sonni kiriting");
+// let sum_1 = 0;
+// let xy;
+// for (xy = 1; xy <= number; xy += 1){
+//   if (xy % 2 == 1){
+//     sum_1 += xy;
+//   }
+// }
+// console.log("Yig'indi: " + sum_1);
+
+// 4-masala
+// let a = +prompt(`4-masala Boshlang'ich sonni kiriting`);
+// let b = +prompt(`4-masala Tugash sonni kiriting`);
+// let res_2 = 1;
+// let c;
+// for (c = a; c <= b; c += 1) {
+//   if (c % 2 == 0) {
+//     res *= c;
+//   }
+// }
+// console.log();(`Ko'paytmasi:`+ res_2)
+
+// 5-masala
+// let num1 = +prompt(`5-masala Boshlang'ich sonni kiriting`);
+// let num2 = +prompt(`5-masala Tugash sonni kiriting`);
+// let res1 =0;
+// let y;
+// for (y = num1; y <= num2; y += 1) {
+//   if (y % 3 == 0 && y % 5 == 0) {
+//     res1 += y;
+//   }
+// }
+// console.log();(`Yig'indi:` + res1);
+
+// 6-masala
+// let number$ = +prompt(`6-masala Ixtiyoriy sonni kiriting`);
+// let x;
+// let result;
+// let sum = 0;
+// for (x = 1; x <= number$; x += 1) {
+//   if (number$ % x == 0) {
+//     sum += 1;
+//     result = sum;
+//   }
+// }
+// console.log(`Yig'indi:` + result);
+
+// 7-masal
+// let number_4 = +prompt(`7-masal Ixtiyoriy sonni kiriting`);
+// let i;
+// let res_4;
+// for (i = 1; i <= number_4; i += 1) {
+//   if (number_4 % i == 0) {
+//   }
+// }
+// console.log(`Yig'indi:` + res_4);
